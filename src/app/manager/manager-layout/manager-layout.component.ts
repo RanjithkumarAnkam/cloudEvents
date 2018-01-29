c4787986ba00bf021491916005ebb4720c89ec99
Ankam Ranjith Kumar <Ranjith@optionmatrix.com>
2018-01-29 12:26:57 +0530

    calendar changes given by sampath

--- src/app/manager/manager-layout/manager-layout.component.ts
+++ src/app/manager/manager-layout/manager-layout.component.ts
@@ -20,13 +20,7 @@ export class ManagerLayoutComponent implements OnInit {
       content: 'This is the heart of the venue management system. All bookings, enquiries, appointments can be managed from here. Events can be added to different time slots as assigned to each venue, payment details for each booking can be managed from the calendar. Different views are provided in the calendar for convenience. Calendar entries can be downloaded and printed for offline use.',
       title: 'Calendar',
       placement: 'bottom',
-    }, 
-    {
-      anchorId: 'Gallery.id',
-      content: "Here you can see the related images and videos of the venues. You can select any venue to view it's images and videos",
-      title: 'Gallery',
-      placement: 'bottom',
-    }, 
+    },      
     {
       anchorId: 'Customers.id',
       content: 'This is where a venue manager can view and manage customer information. Bookings of each customer can be glanced from the grid provided.',
@@ -43,6 +37,12 @@ export class ManagerLayoutComponent implements OnInit {
       anchorId: 'Manage.id',
       content: 'Various venue management essentials can be configured and managed through the Manage tabs. Venues can be created and managed. Staff users and permissions can be configured, vendors and their details can also be managed through these tabs.',
       title: 'Manage',
+      placement: 'bottom',
+    },
+    {
+      anchorId: 'Gallery.id',
+      content: "Here you can see the related images and videos of the venues. You can select any venue to view it's images and videos",
+      title: 'Gallery',
       placement: 'bottom',
     },
     {
