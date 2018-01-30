d6868b614a825445ab1d949a33c49fbcd4ea1bf9
Sai Ramesh Babu Andraju <Ramesh@optionmatrix.com>
2018-01-30 18:19:24 +0530

    final changes given by sampath

--- src/app/manager/subscription/subscription-details/subscription-details.component.ts
+++ src/app/manager/subscription/subscription-details/subscription-details.component.ts
@@ -15,7 +15,14 @@ export class SubscriptionDetailsComponent implements OnInit {
 
   data: any[] = [
     { name: 'Dual Venue', numven: '2', price: 'Rs. 7,000/User' },
-    { name: 'Multiple Venue', numven: '3 - 5', price: 'Rs. 10,000/User' },
+    { name: 'Multiple Venue', numven: '5', price: 'Rs. 10,000/User' },
+  ];
+
+  data2: any[] = [
+    { paymentId: 'PVC001', time: '08-01-2018 - 12:10:42', packagename: 'Single Venue', amount: '70000', status: 'Completed' },
+    { paymentId: 'PVC002', time: '21-01-2018 - 02:31:33', packagename: 'Multiple Venue', amount: '70000', status: 'In Process' },
+    { paymentId: 'PVC003', time: '22-01-2018 - 19:17:21', packagename: 'Dual Venue', amount: '70000', status: 'Failure' },
+    { paymentId: 'PVC004', time: '24-01-2018 - 08:15:56', packagename: 'Single Venue', amount: '70000', status: 'Completed' },
   ];
 
 }
